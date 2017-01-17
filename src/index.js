2c7c0fa178c67a37cf97c2c3158e97ff52b9cf60
alexhisen <ahisen@am.net>
2017-01-16 23:24:14 -0800

    Babel was missing module-exports plugin, so requires didn't work. Also, export * breaks auto-completion after transpilation so don't use that form.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 export MobxSchemaForm from './MobxSchemaForm';
-export * from './validate';
-export * from './schemaFormPropTypes';
+// export * loses auto-completion after transpilation, so specify each export explicitly
+export { getFieldKey, getFieldValue, validateField, validateForm, validateAndSave } from './validate';
+export { modelShape, formShape, mapperShape } from './schemaFormPropTypes';
 export asSchemaField from './asSchemaField';
 export FieldWrapper from './FieldWrapper';
 // We do not export this to allow React-Toolbox to be optional: export SaveButton from './SaveButton';
